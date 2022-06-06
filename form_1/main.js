cfbcb13d2a4a699a2125093ad681afd84215396c
LuisMata <mata.ramirez.1iv14@gmail.com>
2022-06-06 16:58:44 -0500

    form_1

---
+++ form_1/main.js
@@ -0,0 +1,36 @@
+const nombre = document.getElementById("name");
+const usuario= document.getElementById("usr");
+const contraseña = document.getElementById("psw");
+const btn = document.getElementById("btn");
+
+
+let loging ={
+    usr:["luis@pro","david@master"],
+    name:["LUIS","DAVID"],
+    pasword:["contraseña123","david123"]
+}
+
+btn.addEventListener("click",(e)=>{
+    e.preventDefault;
+    validation(usuario.value,nombre.value,contraseña.value);
+});
+
+validation= (usuario,nombre,contraseña)=>{
+    let numeroDeRegistr = loging.usr.indexOf(usuario);
+    if(numeroDeRegistr!=-1){
+        if(loging.name[numeroDeRegistr]==nombre.toUpperCase()){
+            if(loging.pasword[numeroDeRegistr]==contraseña){
+                alert(`Bienvenido ${nombre}`)
+            }else{
+                alert(`contraseña incorrecta`)
+            }
+        }else{
+            alert(`El nombre: ${nombre} no fue encontrado, verifica si esta correcto`)
+        }
+    } else{
+        alert(`El usuario ${usuario} no se encontro, verifique si es correcto`)
+    }
+}
+
+
+
